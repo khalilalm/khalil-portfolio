c2c2f67f35199ba0664b6ba7c098b002bb10e9c1
Khalil Al-Mujeeb <khalilalm@users.noreply.github.com>
2020-07-09 22:36:13 -0400

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/navBar'
+import AboutMe from './components/aboutMe'
+import Projects from './components/projects'
+import Contact from './components/contact'
+import Resume from './components/resume'
+import {BrowserRouter} from 'react-router-dom'
+
 
 function App() {
+
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <NavBar />
+     <AboutMe />
+     <Projects />
+     <Resume />
+     <Contact />
     </div>
+    </BrowserRouter>
   );
 }
 
