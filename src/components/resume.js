34b6639aca142df8079c17de56531b844a350aa1
Khalil Al-Mujeeb <khalilalm@users.noreply.github.com>
2022-10-17 18:47:41 -0400

    fixed resume link

--- src/components/resume.js
+++ src/components/resume.js
@@ -9,7 +9,7 @@ class resume extends Component {
         <div className="resume-wrapper">
           <h1 className="resume-title">Resume</h1>
           <div className="resume-content">
-            <a href="https://docs.google.com/document/d/1qOQatNsrbqK33QUAeYoSmE35K8KXemHl/edit?usp=sharing&ouid=102019679261095126740&rtpof=true&sd=true" target="_blank">
+            <a href="https://docs.google.com/document/d/1YjuZEijqbWI1fG6C1SF_Xi5izQecOmPP/edit?usp=sharing&ouid=102019679261095126740&rtpof=true&sd=true" target="_blank">
               <img className="resume-image" src={resumeI}></img>
             </a>
           </div>
